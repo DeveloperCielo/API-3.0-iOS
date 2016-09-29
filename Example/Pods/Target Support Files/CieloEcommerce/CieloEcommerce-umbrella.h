ba5053a82e3144a14a062619e26006c71e7be485
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2016-09-28 21:49:52 -0300

    Move request objects.

--- Example/Pods/Target Support Files/CieloEcommerce/CieloEcommerce-umbrella.h
+++ Example/Pods/Target Support Files/CieloEcommerce/CieloEcommerce-umbrella.h
@@ -1,16 +1,16 @@
 #import <UIKit/UIKit.h>
 
-#import "EC3Address.h"
-#import "EC3CreditCard.h"
-#import "EC3Customer.h"
 #import "EC3Environment.h"
 #import "EC3Merchant.h"
-#import "EC3Payment.h"
-#import "EC3RecurrentPayment.h"
-#import "EC3Sale.h"
 #import "EC3CieloEcommerce.h"
 #import "EC3JSONAdapter.h"
 #import "EC3RequestModel.h"
+#import "EC3Address.h"
+#import "EC3CreditCard.h"
+#import "EC3Customer.h"
+#import "EC3Payment.h"
+#import "EC3RecurrentPayment.h"
+#import "EC3Sale.h"
 
 FOUNDATION_EXPORT double CieloEcommerceVersionNumber;
 FOUNDATION_EXPORT const unsigned char CieloEcommerceVersionString[];
